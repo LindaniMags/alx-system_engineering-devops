a1da7d2daf727ddfac067ecff361075ca5d5dbdf
LindaniMags <lindanimags@gmail.com>
2024-07-31 19:01:55 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-using an API, for a given employee ID, returns information about his/her TODO list progress.
+Returns todo list progress.
 """
-
 import requests
 import sys
 
