7e69cccdae51cff79a4aa53ddc04b2da05db1c21
LindaniMags <lindanimags@gmail.com>
2024-08-13 07:36:55 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Chrome/51.0.2704.106"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
+
