d3ea1bc85a1295645b168c7dd84d499655937258
LindaniMags <lindanimags@gmail.com>
2024-08-13 13:38:33 -0700

    2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""uses recursion to query API for all titles of hot articles"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    if after is None:
+        return []
+    
+    url = "https://www.reddit.com/r/{}/hot/.json?limit=100&after={}".format(subreddit, after)
+    headers = {"User-Agent": "Chrome/51.0.2704.106"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return None
+    
+    response_json=response.json()
+    hot_articles=response.json.get('data').get('children')
+    for article in hot_articles:
+        hot_list.append(article.get("data").get("title"))
+
+    return hot_articles + recurse(subreddit, [], response_json.get("data").get("after"))
