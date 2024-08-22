41c5f6dc0983cb72841bacab534c83c7bfbf3f48
LindaniMags <lindanimags@gmail.com>
2024-08-22 20:24:35 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,21 +1,12 @@
 # login with the holberton user and open a file without any error message
 
-exec {'replace':
+exec {'replace-1':
   provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
 }
 
-exec {'restart':
+exec {'replace-2':
   provider => shell,
-  command  => 'sudo service nginx restart',
-f high amount of requests
-
-exec {'replace':
-  provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart'],
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
 }
-
-exec {'restart':
-  provider => shell,
